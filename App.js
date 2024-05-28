efc22bc47e49346446e1ce3c263c6fd05e8ca1b9
Philip Clapper <maestro7@gmail.com>
2024-05-28 15:29:40 -0400

    Baseline project

--- App.js
+++ App.js
@@ -1,20 +1,166 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [symbol, setSymbol] = useState('');
+  const [stockData, setStockData] = useState(null);
+  const [predictedPrice, setPredictedPrice] = useState(null);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  const fetchData = async () => {
+    try {
+      const apiKey = 'XERYH0OAOMAV1J0X';
+      const response = await fetch(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`
+      );
+      const data = await response.json();
+
+      if (data['Global Quote']) {
+        setStockData(data['Global Quote']);
+        predictNextDayTradingPrice(data['Global Quote']);
+      } else {
+        console.error('Invalid response format in API call');
+      }
+      
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    const predictNextDayTradingPrice = (stockInfo) => {
+      const latestPrice = parseFloat(stockInfo['05. price']);
+      const changePercent = parseFloat(stockInfo['10. change percent'].replace('%', '')) / 100;
+      const predictedPrice = latestPrice * (1 + changePercent);
+
+      setPredictedPrice(predictedPrice.toFixed(4));
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Stock Market Prediction App
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter Stock Symbol'
+          value={symbol}
+          onChangeText={(text) => setSymbol(text)} />
+        <Pressable style={styles.btn}
+          onPress={fetchData}>
+          <Text style={styles.btnText}>
+            <Text style={styles.animation}>
+            </Text>
+            Get Stock data
+            <Text style={styles.animation}></Text>
+          </Text>
+        </Pressable>
+        {predictedPrice && (
+          <Text style={styles.result}>
+            Predicted Price for Next
+            Trading Day: ${predictedPrice}
+          </Text>
+        )}
+        {stockData && (
+          <View style={styles.stockInfoContainer}>
+            {
+              Object.entries(stockData)
+                .map(([key, value]) => (
+                  <View key={key}
+                    style={styles.stockInfoItem}>
+                    <Text style={styles.stockInfoLabel}>
+                      {key}:
+                    </Text>
+                    <Text style={styles.stockInfoText}>
+                      {value}
+                    </Text>
+                  </View>
+                ))
+            }
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    input: {
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        color: 'white',
+        borderRadius: 5,
+    },
+    btn: {
+        backgroundColor: '#40B3A2',
+        padding: 16,
+        borderRadius: 4,
+        minWidth: 200,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        boxShadowColor: 'rgba(0, 0, 0, 0.1)',
+        boxShadowOffset: { width: 0, height: 4 },
+        boxShadowOpacity: 1,
+        boxShadowRadius: 12,
+        overflow: 'hidden',
+        marginVertical: 10,
+    },
+    btnText: {
+        color: 'white',
+        fontSize: 12,
+        fontWeight: '600',
+        letterSpacing: 1.2,
+        textTransform: 'uppercase',
+        overflow: 'hidden',
+    },
+    animation: {
+        borderRadius: 100,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        height: 80,
+        width: 80,
+        position: 'absolute',
+    },
+    stockInfoContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: 20,
+    },
+    stockInfoItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginBottom: 10,
+        backgroundColor: '#001F3F',
+        padding: 10,
+        borderRadius: 5,
+    },
+    stockInfoLabel: {
+        fontWeight: 'bold',
+        marginRight: 5,
+        color: 'white',
+    },
+    stockInfoText: {
+        color: 'white',
+    },
+    result: {
+        marginTop: 20,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
   },
 });
